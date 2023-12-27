33314a9e326bbf0e046797e36524c3dec3e7e808
3li-3bdullah <PROGRMMER7@gmail.com>
2023-12-27 19:38:42 +0200

    added

---
+++ flutter_adv/lib/modules/login/data/models/login_request_body.dart
@@ -0,0 +1,11 @@
+
+
+@JsonSerializable()
+abstract class LoginRequestBody {
+  final String email;
+  final String password;
+
+  LoginRequestBody({required this.email,required this.password});
+
+  Map<String,dynamic> toJson() => _$LoginRequestBodyToJson(this);
+}
