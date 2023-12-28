8f4d6a034d9dd2b9ab814563ac53fe628fcc8125
3li-3bdullah <PROGRMMER7@gmail.com>
2023-12-28 14:14:41 +0200

    added login cubit

---
+++ flutter_adv/lib/modules/login/logic/cubit/login_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_adv/modules/login/data/models/login_request_body.dart';
+import 'package:flutter_adv/modules/login/data/repos/login_repo.dart';
+import 'package:flutter_adv/modules/login/logic/cubit/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo _loginRepo;
+  LoginCubit(this._loginRepo) : super(const LoginState.initial());
+
+  emitLogin({required LoginRequestBody loginRequestBody }) async {
+    emit(const LoginState.loading());
+    final response = await _loginRepo.login(loginRequestBody);
+    response.when(success: (loginResponse){
+      emit(LoginState.success(loginResponse));
+    }, failure: (failure){
+      emit(LoginState.error(error: failure.apiErrorModel.message ?? 'error' ));
+    });
+  }
+
+}
