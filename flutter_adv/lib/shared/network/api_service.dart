fdf20316284a6991ee825e0321748139cf80852f
3li-3bdullah <PROGRMMER7@gmail.com>
2023-12-28 13:28:15 +0200

    added new features

--- flutter_adv/lib/shared/network/api_service.dart
+++ flutter_adv/lib/shared/network/api_service.dart
@@ -1,8 +1,18 @@
 import 'package:dio/dio.dart';
+import 'package:flutter_adv/modules/login/data/models/login_request_body.dart';
+import 'package:flutter_adv/modules/login/data/models/login_response.dart';
 import 'package:flutter_adv/shared/network/api_constants.dart';
 import 'package:retrofit/retrofit.dart';
+
+part 'api_service.g.dart';
 
 @RestApi(baseUrl: Apiconstants.baseUrl)
 abstract class ApiService{
   factory ApiService(Dio dio,{String baseUrl}) = _ApiService;
+ 
+  @POST(Apiconstants.login)
+  Future<LoginResponse> login(
+    @Body() LoginRequestBody loginRequestBody
+  );
+
 }
