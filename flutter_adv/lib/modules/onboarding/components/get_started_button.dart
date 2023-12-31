6ace0cf5d9b57a135f37ffeac3f5c6f24528961a
3li-3bdullah <PROGRMMER7@gmail.com>
2023-12-31 23:01:26 +0200

    added

---
+++ flutter_adv/lib/modules/onboarding/components/get_started_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_adv/shared/helpers/extentions.dart';
+
+import '../../../shared/routes/routes.dart';
+import '../../../shared/theme/colors.dart';
+import '../../../shared/theme/styles.dart';
+
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all(ColorsManager.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: MaterialStateProperty.all(
+          const Size(double.infinity, 52),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+      ),
+      child: Text(
+        'Get Started',
+        style: TextStyles.font16WhiteSemiBold,
+      ),
+    );
+  }
+}
