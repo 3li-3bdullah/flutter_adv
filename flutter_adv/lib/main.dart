697d444435b00c47c96a311e9c799db1e3e03af0
3li-3bdullah <PROGRMMER7@gmail.com>
2023-12-31 22:56:27 +0200

    added some changes

--- flutter_adv/lib/main.dart
+++ flutter_adv/lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_adv/shared/di/dependency_injection.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  setupGetIt();
   runApp(const MyApp());
 }
 
