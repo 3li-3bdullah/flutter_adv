f981c78821efecd1c7198fd9941c09d75dc752a7
3li-3bdullah <PROGRMMER7@gmail.com>
2023-12-31 22:04:15 +0200

    added get it import

--- flutter_adv/lib/shared/di/dependency_injection.dart
+++ flutter_adv/lib/shared/di/dependency_injection.dart
@@ -4,6 +4,7 @@ import 'package:flutter_adv/modules/login/data/repos/login_repo.dart';
 import 'package:flutter_adv/modules/login/logic/cubit/login_cubit.dart';
 import 'package:flutter_adv/shared/network/api_service.dart';
 import 'package:flutter_adv/shared/network/dio_factory.dart';
+import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
 
