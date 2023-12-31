8b918d27acc11bea79651c88196e724304154a46
3li-3bdullah <PROGRMMER7@gmail.com>
2023-12-31 22:56:57 +0200

    added some vars

--- flutter_adv/lib/modules/login/logic/cubit/login_cubit.dart
+++ flutter_adv/lib/modules/login/logic/cubit/login_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_adv/modules/login/data/models/login_request_body.dart';
 import 'package:flutter_adv/modules/login/data/repos/login_repo.dart';
 import 'package:flutter_adv/modules/login/logic/cubit/login_state.dart';
@@ -6,6 +7,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class LoginCubit extends Cubit<LoginState> {
   final LoginRepo _loginRepo;
   LoginCubit(this._loginRepo) : super(const LoginState.initial());
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
 
   emitLogin({required LoginRequestBody loginRequestBody }) async {
     emit(const LoginState.loading());
