8bca7e8c6daa33f7f323a0adb5b4261bf5242f55
3li-3bdullah <PROGRMMER7@gmail.com>
2024-01-21 23:42:21 +0200

    added

--- flutter_adv/lib/shared/network/api_service.g.dart
+++ flutter_adv/lib/shared/network/api_service.g.dart
@@ -13,7 +13,7 @@ class _ApiService implements ApiService {
     this._dio, {
     this.baseUrl,
   }) {
-    baseUrl ??= 'your base url';
+    baseUrl ??= 'your base url/';
   }
 
   final Dio _dio;
